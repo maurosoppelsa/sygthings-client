5cab6bce1b10a32d421a3f7e74b132ebdc2b5bd7
maurosoppelsa <l.mauro.soppelsa@gmail.com>
2022-05-01 17:25:48 -0300

    dashboard updates

--- app/components/dashboard/dashboard.component.tsx
+++ app/components/dashboard/dashboard.component.tsx
@@ -1,30 +1,82 @@
+import { AppBar, HStack, IconButton, Avatar, Flex, Box } from "@react-native-material/core";
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Button, StyleSheet, View, Text } from 'react-native';
+import { ImageBackground, StyleSheet, View, Text } from 'react-native';
 import colors from '../../config/colors';
+import imageBg from '../../assets/dashboard_bg.jpg';
 
 
 export default function DashboardComponent() {
   return (
-    <View style={styles.container}>
-      <View>
-          <Text>Dashboard</Text>
+    <ImageBackground source={imageBg} resizeMode="cover" style={styles.image}>
+      <StatusBar backgroundColor="#07840a" />
+      <View style={styles.container}>
+        <Box style={styles.sectionContainer}>
+          <AppBar
+            title='Syghtings'
+            style={styles.bar}
+            tintColor={colors.white}
+            leading={() => (
+              <IconButton onPress={() => console.log("to be defined")} icon={<Icon size={30} name="menu" color="white" />} />
+            )}
+            trailing={props => (
+              <HStack>
+                <IconButton
+                  icon={<Avatar label="IARA FIGINI" size={40} color={colors.lightGray} />}
+                  onPress={() => console.log("to be defined")}
+                  {...props}
+                />
+              </HStack>
+            )}
+          />
+          <Box style={styles.mainSection}>
+            <Box style={styles.cameraButton}>
+              <Icon name='camera-plus' size={100} color='#000' />
+              <Text style={styles.newSightLegend}>
+                NEW SIGHT</Text>
+            </Box>
+          </Box>
+        </Box>
       </View>
-      <Button title='Dashboard' onPress={() => alert('Hello')} />
-      <StatusBar style="auto" />
-    </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.primary,
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row"
+  },
+  bar: {
+    flex: 1,
+    paddingTop: 25,
+    maxHeight: 85,
+    backgroundColor: colors.syghtingGreen
   },
   text: {
     fontSize: 30,
-    color: "white"
+    color: colors.white
+  },
+  image: {
+    width: '100%',
+    height: '100%'
+  },
+  mainSection: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: "center"
+  },
+  sectionContainer: {
+    flex: 1,
+    flexDirection: "column"
+  },
+  cameraButton: {
+    alignSelf: "flex-end",
+    alignItems: "center",
+    marginBottom: 20
+  },
+  newSightLegend: {
+    paddingLeft: 12,
   }
 });
--- app/config/colors.ts
+++ app/config/colors.ts
@@ -3,6 +3,7 @@ export default {
     secondary:"#000000",
     black: "#333333",
     gray: '#999999',
+    lightGray: '#d3d3d3',
     syghtingGreen: '#089A0B',
     white: '#FFFDFA',
     blue: '#4E78FF'
