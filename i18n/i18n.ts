591644f141a9665ccef383929ad384b7c379797f
maurosoppelsa <l.mauro.soppelsa@gmail.com>
2023-05-06 13:39:48 -0300

    change i18n library

--- i18n/i18n.ts
+++ i18n/i18n.ts
@@ -1,15 +1,38 @@
-import I18n from 'react-native-i18n';
+import i18next from 'i18next';
 import { defaultLanguage } from '../app/config/language';
-const  en = require('./locales/en.json');
-const  es = require('./locales/es.json');
+const en = require('./locales/en.json');
+const es = require('./locales/es.json');
 
-I18n.fallbacks = true;
+class I18n {
+  static instance: I18n;
 
-I18n.defaultLocale = defaultLanguage;
- 
-I18n.translations = {
-  en,
-  es,
-};
- 
-export default I18n;
+  constructor() {
+    if (I18n.instance) {
+      return I18n.instance;
+    }
+
+    i18next.init({
+      lng: defaultLanguage,
+      fallbackLng: defaultLanguage,
+      compatibilityJSON: 'v3',
+      resources: {
+        en: {
+          translation: en,
+        },
+        es: {
+          translation: es,
+        },
+      },
+    });
+
+    I18n.instance = this;
+  }
+
+  t(key: string) {
+    return i18next.t(key);
+  }
+}
+
+const i18nInstance = new I18n();
+
+export default i18nInstance;
