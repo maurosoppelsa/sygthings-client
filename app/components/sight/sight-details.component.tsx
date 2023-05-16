58ba603591be5d537f75e417508c406533ccdd2d
maurosoppelsa <l.mauro.soppelsa@gmail.com>
2023-05-15 21:07:43 -0300

    edit sight part I

--- app/components/sight/sight-details.component.tsx
+++ app/components/sight/sight-details.component.tsx
@@ -7,52 +7,75 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { capitalizeText, getCreatedByLegend } from '../../utils/common';
 import ActionModalComponent from '../common/action-modal.component';
 import I18n from '../../../i18n/i18n';
+import SightEditComponent from './sight-edit.component';
 
 
 export default function SightDetailsComponent({ sight, onClose, allowDelete = false, onDelete, onUpdate }: { sight: Sight, onClose: any, allowDelete?: boolean, onDelete?: any, onUpdate?: any }) {
     const { name, lastName, occupation } = sight?.user ?? {};
     const [showModal, setShowModal] = React.useState(false);
-    return (
-        <Box style={styles.container}>
-            <Image style={styles.sightImage} source={{ uri: sight?.picture.uri }} />
-            <TouchableOpacity style={[styles.touchableHeader, styles.touchableBackBtWrapper]} onPress={() => { onClose() }}>
-                <MaterialCommunityIcons name="arrow-left" size={40} style={styles.headerButton} />
-            </TouchableOpacity>
-            {
-                allowDelete &&
-                <Box style={[styles.touchableActionContainer, styles.touchableHeader]}>
-                    <TouchableOpacity onPress={() => { setShowModal(true) }}>
-                        <MaterialCommunityIcons name="trash-can-outline" size={30} style={[styles.headerButton, styles.actionButton]} />
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => { onUpdate() }}>
-                        <MaterialCommunityIcons name="border-color" size={30} style={[styles.headerButton, styles.actionButton]} />
-                    </TouchableOpacity>
+    const [showEdit, setShowEdit] = React.useState(false);
+
+    const updateSight = (sight: Sight) => {
+        setShowEdit(false);
+        onUpdate(sight);
+    }
+
+    const renderDetails = () => {
+        return (
+            <Box style={styles.container}>
+                <Image style={styles.sightImage} source={{ uri: sight?.picture.uri }} />
+                <TouchableOpacity style={[styles.touchableHeader, styles.touchableBackBtWrapper]} onPress={() => { onClose() }}>
+                    <MaterialCommunityIcons name="arrow-left" size={40} style={styles.headerButton} />
+                </TouchableOpacity>
+                {
+                    allowDelete &&
+                    <Box style={[styles.touchableActionContainer, styles.touchableHeader]}>
+                        <TouchableOpacity onPress={() => { setShowModal(true) }}>
+                            <MaterialCommunityIcons name="trash-can-outline" size={30} style={[styles.headerButton, styles.actionButton]} />
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => { setShowEdit(true) }}>
+                            <MaterialCommunityIcons name="border-color" size={30} style={[styles.headerButton, styles.actionButton]} />
+                        </TouchableOpacity>
+                    </Box>
+                }
+                <Box style={styles.headerContainer}>
+                    <Text style={styles.sightName}>{sight?.animal}</Text>
+                    <Box style={styles.locationContainer}>
+                        <MaterialCommunityIcons name="map-marker" size={22} style={styles.locationIcon} />
+                        <Text>{sight?.placeName}</Text>
+                    </Box>
                 </Box>
-            }
-            <Box style={styles.headerContainer}>
-                <Text style={styles.sightName}>{sight?.animal}</Text>
-                <Box style={styles.locationContainer}>
-                    <MaterialCommunityIcons name="map-marker" size={22} style={styles.locationIcon} />
-                    <Text>{sight?.placeName}</Text>
+                <Divider style={styles.divider} />
+                <Box style={styles.detailsContainer}>
+                    <Text>Condition: {capitalizeText(sight?.condition)}</Text>
+                    <Text>Description: <Text style={styles.description}>{sight?.description}</Text></Text>
+                    <Text style={styles.createdText}>
+                        {getCreatedByLegend(name, lastName, occupation, sight?.createdAt)}
+                    </Text>
                 </Box>
-            </Box>
-            <Divider style={styles.divider} />
-            <Box style={styles.detailsContainer}>
-                <Text>Condition: {capitalizeText(sight?.condition)}</Text>
-                <Text>Description: <Text style={styles.description}>{sight?.description}</Text></Text>
-                <Text style={styles.createdText}>
-                    {getCreatedByLegend(name, lastName, occupation, sight?.createdAt)}
-                </Text>
-            </Box>
-            <ActionModalComponent
+                <ActionModalComponent
                     showModal={showModal}
-                    actionBtText={I18n.t('Common.delete')} 
+                    actionBtText={I18n.t('Common.delete')}
                     actionCancel={() => setShowModal(false)}
                     actionProceed={() => onDelete()}
                     title={I18n.t('Sight.deleteTitle')}
                     subtitle={I18n.t('Sight.deleteSubtitle')} />
-        </Box>
-    );
+            </Box>
+        );
+    }
+    const renderEdit = () => {
+        return (
+            <Box style={styles.container}>
+                <SightEditComponent sight={sight} onUpdateSight={updateSight} onCancelUpdate={() => { setShowEdit(false) }} />
+            </Box>
+        );
+    }
+    if (showEdit) {
+        return renderEdit();
+    }
+    else {
+        return renderDetails();
+    }
 }
 
 const styles = StyleSheet.create({
---
+++ app/components/sight/sight-edit.component.tsx
@@ -0,0 +1,145 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, Image, TextInput } from "react-native";
+import { Box, Button } from "@react-native-material/core";
+import { Sight } from "../../interfaces/common";
+import colors from "../../config/colors";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { RadioButton } from 'react-native-paper';
+import I18n from '../../../i18n/i18n';
+
+export default function EditSightComponent({ sight, onCancelUpdate, onUpdateSight }: { sight: Sight, onCancelUpdate: any, onUpdateSight: any }) {
+    const [animalName, setAnimalName] = useState(sight?.animal);
+    const [description, setDescription] = useState(sight?.description);
+    const alive = I18n.t('NewSightForm.alive');
+    const wounded = I18n.t('NewSightForm.wounded');
+    const dead = I18n.t('NewSightForm.dead');
+    const [checked, setChecked] = React.useState(sight?.condition);
+
+    return (
+        <Box style={styles.container}>
+            <Box style={styles.header}>
+                <MaterialCommunityIcons style={styles.icon} name="border-color" size={25} />
+                <Text style={styles.title}>{I18n.t('EditSight.title')}</Text>
+            </Box>
+            <Box style={styles.imageContainer}>
+                <Image style={styles.image} source={{ uri: sight?.picture.uri }} />
+                <Button title={I18n.t('EditSight.updatePicture')} style={[styles.button, styles.updatePictureBT]} onPress={() => { }} />
+            </Box>
+            <Box>
+                <Text style={styles.inputTitle}>{I18n.t('EditSight.animalName')}</Text>
+                <TextInput style={styles.sightInputs} value={animalName} onChangeText={(value) => { setAnimalName(value) }} />
+                <Text style={styles.inputTitle}>{I18n.t('EditSight.description')}</Text>
+                <TextInput
+                    multiline
+                    numberOfLines={4}
+                    style={[styles.sightInputs, styles.inputDesc]} value={description}
+                    onChangeText={(value) => { setDescription(value) }} />
+            </Box>
+            <Box>
+                <Text style={styles.inputTitle}>{I18n.t('EditSight.condition')}</Text>
+                <Box style={styles.radioContainer}>
+                    <Text style={styles.radioTitle}>{alive}</Text>
+                    <RadioButton
+                        value={alive}
+                        status={checked === alive ? 'checked' : 'unchecked'}
+                        onPress={() => setChecked(alive)}
+                    />
+                    <Text style={styles.radioTitle}>{wounded}</Text>
+                    <RadioButton
+                        value={wounded}
+                        status={checked === wounded ? 'checked' : 'unchecked'}
+                        onPress={() => setChecked(wounded)}
+                    />
+                    <Text style={styles.radioTitle}>{dead}</Text>
+                    <RadioButton
+                        value={dead}
+                        status={checked === dead ? 'checked' : 'unchecked'}
+                        onPress={() => setChecked(dead)}
+                    />
+                </Box>
+            </Box>
+            <Box style={styles.actionButtonContainer}>
+                <Button title={I18n.t('Common.edit')} style={[styles.button, styles.actionBT]} onPress={() => onCancelUpdate()} />
+                <Button title={I18n.t('Common.cancel')} style={[styles.button, styles.actionBT]} onPress={() => onUpdateSight()} />
+            </Box>
+        </Box>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        padding: 10,
+        alignItems: 'center',
+    },
+    header: {
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    icon: {
+        alignSelf: 'center',
+        marginTop: 5,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        alignSelf: 'center',
+        marginLeft: 5,
+        marginBottom: 5,
+    },
+    imageContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+    image: {
+        width: 300,
+        height: 300,
+        borderWidth: 2,
+        borderColor: colors.gray,
+        borderRadius: 5,
+    },
+    updatePictureBT: {
+        width: 300,
+        marginTop: 10,
+    },
+    actionButtonContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: 300,
+    },
+    button: {
+        borderRadius: 5,
+        backgroundColor: colors.syghtingGreen,
+        marginTop: 30,
+    },
+    inputTitle: {
+        fontWeight: 'bold',
+        marginTop: 10,
+    },
+    sightInputs: {
+        width: 300,
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginTop: 10,
+        padding: 5,
+    },
+    inputDesc: {
+        height: 100,
+    },
+    actionBT: {
+        width: 140,
+    },
+    radioContainer: {
+        flexDirection: "row",
+        marginTop: 10,
+        alignItems: 'flex-start',
+        justifyContent: 'space-between',
+        width: 300,
+    },
+    radioTitle: {
+        marginTop: 8,
+    },
+});
